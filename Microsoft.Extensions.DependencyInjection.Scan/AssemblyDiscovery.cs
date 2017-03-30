b8bfe4aa57d6f3448db42c9f5e288bca32a046f1
duxianpeng <539374843@qq.com>
2017-03-30 10:39:55 +0800

    feat:AssemblyDiscovery

---
+++ Microsoft.Extensions.DependencyInjection.Scan/AssemblyDiscovery.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.DotNet.PlatformAbstractions;
+using Microsoft.Extensions.DependencyModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection.Scan
+{
+    public class AssemblyDiscovery
+    {
+        public static Assembly[] Discovery()
+        {
+           
+            return DependencyContext.Default.RuntimeLibraries.SelectMany(l => l.GetDefaultAssemblyNames(DependencyContext.Default)).Select(Assembly.Load).ToArray();
+        }
+    }
+}
